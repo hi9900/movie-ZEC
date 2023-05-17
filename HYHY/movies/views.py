4ffefa701c74a8efcc2bf350d9f661f2f43035bf
HY <9hiyoung@gmail.com>
2023-05-18 00:29:10 +0900

    REMOVE Genre get post

--- HYHY/movies/views.py
+++ HYHY/movies/views.py
@@ -4,21 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from .models import Movie, Genre
-from .serializers import MovieSerializer, GenreSerializer
-
-
-@api_view(['GET', 'POST'])
-def genre_list(request):
-    if request.method == 'GET':
-        genres = Genre.objects.all()
-        serializer = GenreSerializer(genres, many=True)
-        return Response(serializer.data)
-    elif request.method == 'POST':
-        serializer = GenreSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+from .serializers import MovieSerializer
 
 
 # Create your views here.
