805c997da8ff98987b90fd834e24826d402cfd25
HY <9hiyoung@gmail.com>
2023-05-19 03:38:22 +0900

    UPDATE vue app_bar

--- front-final/src/router/index.js
+++ front-final/src/router/index.js
@@ -4,7 +4,21 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 const routes = [
-
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import('@/views/HomePageView')
+  },
+  {
+    path: '/movie',
+    name: 'Movie',
+    component: () => import('@/views/MoviePageView')
+  },
+  {
+    path: '/community',
+    name: 'Community',
+    component: () => import('@/views/CommunityPageView')
+  },
 ]
 
 const router = new VueRouter({
