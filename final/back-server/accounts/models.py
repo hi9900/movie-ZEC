172efd3714d97f6b2c3e8d79363e73e1b5bd9301
HB Kim <ghkdwp129@gmail.com>
2023-05-23 17:59:30 +0900

    I do but I don't know

--- final/back-server/accounts/models.py
+++ final/back-server/accounts/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import  AbstractUser, User #AbstractBaseUser, BaseUserManager, PermissionsMixin,
+from django.contrib.auth.models import  AbstractUser # User #AbstractBaseUser, BaseUserManager, PermissionsMixin,
 from django.contrib.auth import get_user_model
 from django.conf import settings
 
